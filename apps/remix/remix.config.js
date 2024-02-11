0c398f1dab551235e1104771b94d1211d396be02
Jack Clackett <jack@noquarter.co>
2024-02-11 19:19:34 +0100

    init

---
+++ apps/remix/remix.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+const { flatRoutes } = require("remix-flat-routes")
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  serverModuleFormat: "cjs",
+  serverBuildTarget: "cjs",
+}
